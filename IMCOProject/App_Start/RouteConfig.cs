b06362bce28df784d4c9bd108b6c0ef872154e36
imco <alfarsi.talal@gmail.com>
2020-02-22 17:19:34 +0530

    small corrections

--- IMCOProject/App_Start/RouteConfig.cs
+++ IMCOProject/App_Start/RouteConfig.cs
@@ -12,12 +12,17 @@ namespace IMCOProject
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+    //        routes.MapRoute(
+    //    "Area",
+    //    "",
+    //    new { area = "Student", controller = "Student", action = "Index" }
+    //);
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
